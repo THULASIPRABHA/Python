06b8e1ecbc9750ade6222ab6ab527565484df50c
THULASIPRABHA <69714505+THULASIPRABHA@users.noreply.github.com>
2021-01-06 21:33:01 +0530

    Add files via upload

---
+++ invert number triangle.py
@@ -0,0 +1,18 @@
+row=5
+while row>=1:
+ col=5
+ while col>row:
+   print(" ",end='')
+   col-=1
+ col2=1
+ while col2<=row:
+     print(col,' ',end='')
+     col2+=1
+ 
+ print()
+ row-=1
+
+
+ 
+
+    
